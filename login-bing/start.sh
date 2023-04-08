10bd33afda08dab3a5b051f080e73d05bdbcdb9d
linghaihui <75124771@qq.com>
2023-04-08 14:26:10 +0800

    Fix start.sh

--- login-bing/start.sh
+++ login-bing/start.sh
@@ -3,8 +3,8 @@ if [ $count -gt 0 ]; then
   docker stop login-bing && docker rm login-bing
 fi
 
-if [ -f cron.log ];then
+if [ ! -f cron.log ]; then
   touch cron.log
 fi
 
-docker run --name login-bing -d -v $(pwd)/cronfile:/etc/cron.d/loginbing -v $(pwd)/cookies:/bing/cookies -v $(pwd)/cron.log:/bing/cron.log  -v $(pwd)/env:/bing/env yy194131/login-bing:$1
+docker run --name login-bing -d -v $(pwd)/cronfile:/etc/cron.d/loginbing -v $(pwd)/cookies:/bing/cookies -v $(pwd)/cron.log:/bing/cron.log -v $(pwd)/env:/bing/env yy194131/login-bing:$1
